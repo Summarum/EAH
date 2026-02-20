f863d0c7043d4cd38cdec71e3017ac4cb919a5b0
Summarum <revagerek@gmail.com>
2026-02-20 18:12:27 +0100

    added CashFlow Model

---
+++ Models/ModelCashFlow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EAH.Models
+{
+    internal class ModelCashFlow
+    {
+        public int period { get; set; }
+        public double amount { get; set; }
+    }
+}
